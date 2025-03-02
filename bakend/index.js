6514e591a242b478fae83542ca53f458d745665e
mike-ki17 <michael17ycd@gmail.com>
2025-03-02 18:18:47 -0500

    remove node_modules del commit

--- bakend/index.js
+++ bakend/index.js
@@ -4,6 +4,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hola, Express con ESM!");
 });
+app.get("/consulta", (req, res) => {
+  res.send("consulta");
+});
 
 // Se crea un servidor en el puerto 3000
 app.listen(3000, () => {
