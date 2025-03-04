5bc6396f88c734004e7a58e457e2444ea0746b3f
mike-ki17 <michael17ycd@gmail.com>
2025-03-04 13:45:44 -0500

    install react icons

---
+++ frontend/Tatu/src/components/Nav/Nav.jsx
@@ -0,0 +1,24 @@
+
+import logo from '../../../public/img/Logo _ ART BYTE_White.png'
+
+function Nav() {
+  return (
+    <div className="container-nav">
+        <div className="content-items-nav">
+          <div className="logo-tatu-nav">
+
+          </div>
+          <div className="list-items-nav">  
+
+          </div>
+
+          <div className="btn-menu-nav">
+
+          </div>
+        </div>
+    </div>
+
+  )
+}
+
+export default Nav
