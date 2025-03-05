bc26fafdac896c5fc6e56d406b1a3cc31341d44c
Juan Felipe Higuera <163941029+juan-higuera@users.noreply.github.com>
2025-03-05 14:34:17 -0500

    CardUser
    add the carduser with maluma

---
+++ frontend/Tatu/src/components/CardUser/CardUser.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './CardUser.css';
+
+function CardUser() {
+  const [user, setUser] = useState([
+    { nombre: 'juan' },
+    { nombre: 'mike' },
+    { nombre: 'gerald' },
+    { nombre: 'cristian' }  
+  ]);
+
+  return (
+    <div>
+      {user.map((usuario, index) => (
+        <div className="container-card" key={index}>
+          <div className="info-user-card">
+            <img src="/img/maluma.jpg" alt="Perfil" />
+            <span>{usuario.nombre}</span>
+            <span>Recomendación de tatuajes de chico pretty boy</span>
+          </div>
+          <div className="btn-seguir"> 
+            <div>Seguir</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default CardUser;
