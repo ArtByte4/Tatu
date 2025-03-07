d2264782485d3342d0073955d06f8ca20e996ebf
mike-ki17 <michael17ycd@gmail.com>
2025-03-07 16:29:55 -0500

    Connection base de datos exitosa

--- bakend/index.js
+++ bakend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { getUsers } from "./consulta.js";
 const app = express();
 
 app.get("/", (req, res) => {
@@ -11,4 +12,13 @@ app.get("/consulta", (req, res) => {
 // Se crea un servidor en el puerto 3000
 app.listen(3000, () => {
   console.log("Servidor corriendo en http://localhost:3000");
+});
+
+app.get("/users", (req, res) => {
+  getUsers((err, users) => {
+    if (err) {
+      return res.status(500).json({ error: "Error al obtener usuarios" });
+    }
+    res.json(users);
+  });
 });
