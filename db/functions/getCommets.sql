c1531f387f15d68057575caeb85c7b00b7799ffe
mike-ki17 <michael16cd@gmail.com>
2025-03-10 15:56:55 -0500

    change function getCommets

--- db/functions/getCommets.sql
+++ db/functions/getCommets.sql
@@ -1,13 +1,21 @@
 
 
-DELIMITER //
-CREATE PROCEDURE getComments()
-BEGIN
-    SELECT comments.*, users.*
-    FROM comments
-    JOIN users ON comments.user_id = users.user_id;
-END 
+delimiter //
+
+create function getUserComments (pepesaurio int)
+returns text deterministic
+begin
+declare comentarios text;
+bucle: LOOP
+    SET pepesaurio pepesaurio + 1;
+    select GROUP_CONCAT(content SEPARATOR |||||) into comentarios
+    from comments JOIN users ON comments.user id users.user id
+    where users.user_id = pepesaurio;
+IF pepesaurio > 10 THEN
+LEAVE bucle;
+end if;
+end loop;
+return comentarios;
+end
+
 //
-
-
-
