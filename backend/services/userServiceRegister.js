09f1d6837f55f5f21269535924ba83ab3831fb94
mike-ki17 <michael16cd@gmail.com>
2025-03-14 12:18:56 -0500

    Encriptacion de passwords en el registro

--- backend/services/userServiceRegister.js
+++ backend/services/userServiceRegister.js
@@ -1,5 +1,13 @@
 
 
 import bcrypt from 'bcrypt';
-import { addUser } from '../models/addUser'
+import { addUser } from '../models/insertarDatos.js';
 
+
+const createUser = async (dataUser) => {
+    dataUser.password_hash = await bcrypt.hash(dataUser.password_hash, 13);
+    return addUser(dataUser)
+}
+
+
+export { createUser };
