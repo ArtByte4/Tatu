6b30ecb5f0b640768aeeb1bf45548b29bf927e19
mike-ki17 <michael16cd@gmail.com>
2025-03-18 19:09:18 -0500

    Page Perfil User succefull

--- frontend/Tatu/src/components/PerfilUser/PerfilUser.jsx
+++ frontend/Tatu/src/components/PerfilUser/PerfilUser.jsx
@@ -1,42 +1,73 @@
 import "./PerfilUser.css";
-
+import { TbNut } from "react-icons/tb";
+import { MdPhotoCamera } from "react-icons/md";
+import { useRef } from "react";
 function PerfilUser() {
+
+  const fileInputRef = useRef(null);
+
+  const handleFileChange = (e) => {
+    console.log(e.target.files)
+  }
+
+
+  const handleUploadFile = () => {
+    console.log('yeah')
+    fileInputRef.current.click();
+  }
+
   return (
     <div className="container_perfilUser">
       <div className="content_perfilUser">
         <div className="seccion_info_perfilUser">
-          <div className="container_img_perfilUser">
-            <img src="./../../public/img/user_default.png" alt="" />
+          <div className="container-img-perfilUser">
+            <button onClick={handleUploadFile}>
+            <img src="./../../public/img/user_default2.png" alt="" />
+            <MdPhotoCamera className="img-photo" color="#fff" />
+            </button>
+            <form action="">
+              <input type="file" name="photo-perfil" ref={fileInputRef} onChange={handleFileChange} />
+            </form>
           </div>
           <div className="section_container_perfilUser">
             <div className="items_perfilUser">
-              <span>mikeki</span>
-              <div className="btn_edit_perfil">
-                <a href="#">Editar perfil</a>
+              <div className="item">
+                <div className="btn-info-account">
+                <span>mikeki</span>
+                </div>
+                <div className="btn_edit_perfil">
+                  <a href="#">Editar perfil</a>
+                </div>
+                <div className="btn-config">
+                <TbNut fontSize={25}/>
               </div>
+              </div>
+              
             </div>
-            <section className="list_describe_perfilUser">
+            <div className="list_describe_perfilUser">
               <ul>
                 <li>
                   <div className="item_content">
-                    <span>0</span>
-                    <span>Publicaciones</span>
+                    <span className="num">0</span>
+                    <span className="describe_item">Publicaciones</span>
                   </div>
                 </li>
                 <li>
                   <div className="item_content">
-                    <span>0</span>
-                    <span>Seguidores</span>
+                    <span className="num">0</span>
+                    <div className="describe_item"><span>
+                      Seguidores</span></div>
                   </div>
                 </li>
                 <li>
                   <div className="item_content">
-                    <span>0</span>
-                    <span>seguidos</span>
+                    <span className="num">0</span>
+                    <div className="describe_item"><span>
+                    Seguidos</span></div>
                   </div>
                 </li>
               </ul>
-            </section>
+            </div>
           </div>
         </div>
       </div>
