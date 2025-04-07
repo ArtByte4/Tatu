2134a428421926dffc5c7caba21300095d520a0b
mike-ki17 <michael16cd@gmail.com>
2025-04-07 10:38:00 -0500

    refactor estructura basada en features

---
+++ frontend/Tatu/src/features/explore/components/PeopleExplore.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import "./PeopleExplore.css";
+import axios from "axios";
+function PeopleExplore() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true); // Estado para indicar carga
+  const [error, setError] = useState(null); // Estado para manejar errores
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/users");
+      setUsers(response.data);
+    } catch (err) {
+      console.error("Error al obtener usuarios:", err);
+      setError("Hubo un problema al cargar los usuarios.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers(); // Cargar datos al montar el componente
+  }, []);
+
+  // Mensaje de carga
+  if (loading) return <p>Cargando usuarios...</p>;
+
+  // Mensaje de error
+  if (error) return <p>{error}</p>;
+  console.log(users);
+  return (
+    <div className="container_suggested">
+      <div className="content_suggested">
+        <div className="title_seccion">
+          <h4>Sugerencias</h4>
+        </div>
+
+        <div className="main_content">
+          {users.map((user) => {
+            return (
+              <div className="card_item_suggested" key={user.user_handle}>
+                <div className="card_user">
+                  <div className="conten-img">
+                    <img src="../../../public/img/maluma.jpg" alt="" />
+                  </div>
+                  <div className="describe_item_suggested">
+                    <span className="user_handle">{user.user_handle}</span>
+                    <span className="text">Sugerencia para ti</span>
+                  </div>
+                </div>
+                <button className="seguir_suggested">Seguir</button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PeopleExplore;
---
+++ frontend/Tatu/src/features/registration/components/Steps/StepTwo.jsx
@@ -0,0 +1,78 @@
+import { useState, useEffect } from "react";
+import PropTypes from 'prop-types';
+
+function StepTwo({formData, setFormData, nexStep, prevStep}) {
+  const [localData, setLocalData] = useState({
+    phonenumber: formData.phonenumber || "",
+    user_handle: formData.user_handle || "",
+    password_hash: formData.password_hash || "",
+  });
+
+  useEffect(() => {
+    const { phonenumber, user_handle, password_hash } = localData;
+    const isFormValid =
+      phonenumber.trim().length > 7 && // Mínimo 7 caracteres en el telefono
+      user_handle.trim().length > 2 && // Mínimo 2 caracteres en el usuario
+      password_hash.trim().length >= 6; // password mínimo 6 caracteres
+
+    setIsValid(isFormValid);
+  }, [localData]);
+
+  const [isValid, setIsValid] = useState(false);
+  const handleChange = (e) => {
+    setLocalData({ ...localData, [e.target.name]: e.target.value });
+  };
+
+  const handleNext = () => {
+    setFormData({ ...formData, ...localData });
+    nexStep();
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        name="phonenumber"
+        placeholder="Número de teléfono"
+        onChange={handleChange}
+        value={localData.phonenumber}
+        
+      />
+      <input
+        type="text"
+        name="user_handle"
+        placeholder="Nombre de usuario"
+        onChange={handleChange}
+        value={localData.user_handle}
+        
+      />
+      <input
+        type="password"
+        name="password_hash"
+        placeholder="contraseña"
+        onChange={handleChange}
+        value={localData.password_hash}
+        
+      />
+       <button className={isValid ? "next-step" : "next-step-invalid"} disabled={!isValid} onClick={handleNext}>
+        Siguiente
+      </button>
+      <button className="prev-step" onClick={prevStep}>
+        Atras
+      </button>
+    </>
+  );
+}
+
+StepTwo.propTypes = {
+  formData: PropTypes.shape({
+    phonenumber: PropTypes.string,
+    user_handle: PropTypes.string,
+    password_hash: PropTypes.string,
+  }).isRequired,
+  setFormData: PropTypes.func.isRequired,
+  nexStep: PropTypes.func.isRequired,
+  prevStep: PropTypes.func.isRequired,
+};
+
+export default StepTwo;
