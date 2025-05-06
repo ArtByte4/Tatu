1b55b9f1ccd1285e96dbf7ec145f92900af70ceb
mike-ki17 <michael16cd@gmail.com>
2025-05-05 19:36:18 -0500

    feature(auth): crear validaciones

--- frontend/Tatu/src/features/auth/actions/login.ts
+++ frontend/Tatu/src/features/auth/actions/login.ts
@@ -1,4 +1,23 @@
 
-export const login = () => {
+import { FormState } from "../types/formTypes";
+import { LoginSchema } from '../validation/loginValidation';
+import { useAuth } from "../hooks/useAuth";
 
+
+
+export const login = async (_state: FormState, formData: FormData) => {
+    const { performLogin } = useAuth();
+    const validatedFields = LoginSchema.safeParse({
+        user: formData.get("user_handle"),
+        password: formData.get("password_hash"),
+    });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+        };
+    }
+
+    const response = await performLogin(validatedFields.data);
+  
 }
--- frontend/Tatu/src/features/auth/validation/loginValidation.ts
+++ frontend/Tatu/src/features/auth/validation/loginValidation.ts
@@ -1,7 +1,7 @@
 // src/features/auth/schemas/loginSchema.ts
 import { z } from "zod";
 
-export const loginSchema = z.object({
+export const LoginSchema = z.object({
   user_handle: z
     .string()
     .min(3, "El nombre de usuario debe tener al menos 3 caracteres")
@@ -11,4 +11,4 @@ export const loginSchema = z.object({
     .min(6, "La contraseña debe tener al menos 6 caracteres"),
 });
 
-export type LoginSchema = z.infer<typeof loginSchema>;
+export type LoginSchema = z.infer<typeof LoginSchema>;
